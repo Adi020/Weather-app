c1a2490b2a489d52e784543707db9659c9923554
Fabio Adrian <adrian.camacho.zotelo@gmail.com>
2023-05-28 22:19:23 -0600

    first commit

---
+++ src/components/Loader.jsx
@@ -0,0 +1,20 @@
+import "./style/Loader.css"
+
+const Loader = () => {
+  return (
+    <div className="lds-spinner"><div>
+        </div><div>    
+        </div><div>
+        </div><div>
+        </div><div>
+        </div><div>
+        </div><div>
+        </div><div>
+        </div><div>
+        </div><div>
+        </div><div>
+        </div><div>
+        </div></div>
+  )
+}
+export default Loader
