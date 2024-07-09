e39bd0f71e7c8914946818bd032e3f3614764db7
Adi <121740959+Adi020@users.noreply.github.com>
2024-07-09 15:04:15 -0600

    Update Loader.jsx

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,7 +2,7 @@ import "./style/Loader.css";
 
 const Loader = () => {
   return (
-    <div className="lds-spinner">
+    <div className="lds-spinner absolute top-[50%] -translate-y-1/2 left-[50%] -translate-x-1/2">
       <div></div>
       <div></div>
       <div></div>
